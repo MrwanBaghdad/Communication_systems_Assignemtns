aef3a3ecbb9be6eb31700c51f1a38425c677f27d
marwan <mrwanbaghdad76@gmail.com>
2017-10-22 20:05:00 +0200

    correct snr generation

--- assignment_1/assignment1.m
+++ assignment_1/assignment1.m
@@ -1,9 +1,9 @@
-
+close all;
 [signal,Fs] = audioread('malcolm_x.wav');
 
 
 
-%sample sound to 8000 sample/sec
+%% sample sound to 8000 sample/sec
 sampled=resample(signal,8000,Fs);
 t=length(sampled);
 t=linspace(0,15,t);
@@ -28,13 +28,15 @@ quant=[];
     quant(i)= round(y(i)*2^(K-1))/(2^(K-1))-sign(y(i))*q/2;
   end
 quantiz = @(x,k)  ((max(x) - min(x)) / (2^(k) -1) ) * floor( (x /  ((max(x) - min(x)) / (2^(k) -1 ) ) ) + 0.5);
-quant=quantiz(y, 8);
+delta = ((max(x) - min(x)) / (2^(k) -1) )
+quant = uencode(y,8);
+quant = double(quant);
 quantnoise=mean((y-quant).^2);
 fprintf('\n mean square error for uniform quantization = %g',quantnoise)
 figure;
 plot(t,y)
 title('Uniform quantization')
-  %A-law compandor
+  %% A-law compandor
   A1=[];
   A1 = compand(y,10,ymax,'A/compressor');
   A1=A1';
@@ -56,7 +58,7 @@ title('Uniform quantization')
  title('values of A vs mean square error')
  xlabel('A values')
  ylabel('mean square error')
- %U-law compandor
+ %% U-law compandor
    U1=[];
   U1 = compand(y,10,ymax,'mu/compressor');
   quantnoise=[];
@@ -77,28 +79,15 @@ title('Uniform quantization')
  xlabel('U values')
  ylabel('mean square error')
 
- %%% Quantization of A2 
- 
-a2_quantized = quantiz(A2,8);
+ %% Quantization of A2 
 
-encode = @(x,k) ( x - min(x)) / (max(x) - min(x))  * ( 2 ^ k - 1);
-pcm_a2 = encode(a2_quantized,8);
+pcm = uencode(A2, 8);
+SNR = 10:2:30;
+ber = []    
+for i= 1:length(SNR)
+[n, b] = biterr(de2bi(pcm), de2bi(qamdemod(awgn(qammod(pcm, 256, 0,'bin'), SNR(i),'measured'), 256, 0,'bin')));
+ber(i) = b;
 
-
-noised_quantized_signals = zeros(10, length(pcm_a2));
-yk = zeros(10, length(pcm_a2));
-ber = []
-snr = []
-q = @(x, k)(max(x) - min(x) )/  (2^k -1);
-
-%TODO: multidimensions array for holding data
-
-for i = 1:2
-    i
-    noised_quantized_signals(i,:) = zeros(length(pcm_a2));
-    noised_quantized_signals(i,:) = awgn(pcm_a2, 10 + i*2);
-    yk(i) = noised_quantized_signals * q(pcm_a2,8);
-    ber(i) = biterr(yk, y);
-    snr(i) = snr(yk,y);
+% reconstruction 
 end
-
+plot(SNR, ber)
